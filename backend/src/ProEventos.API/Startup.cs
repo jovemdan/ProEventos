085dc6e00a391ecf615ae9121d39a98531bd5653
Danilo Gonçalves <twiixplays@gmail.com>
2021-03-29 19:14:53 -0300

    Seção 4: Angular

--- backend/src/ProEventos.API/Startup.cs
+++ backend/src/ProEventos.API/Startup.cs
@@ -32,6 +32,7 @@ namespace ProEventos.API
                 context => context.UseSqlite(Configuration.GetConnectionString("Default"))
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -53,6 +54,11 @@ namespace ProEventos.API
             app.UseRouting();
 
             app.UseAuthorization();
+
+            //autoriza acessar a API
+            app.UseCors(x=>x.AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
